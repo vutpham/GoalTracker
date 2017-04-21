068a041691ac559c2f962ceb545690823db3bcaf
Vu Pham <vutpham@outlook.com>
2017-04-21 11:22:56 -0700

    Create specs for User#find_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+  validates :name, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def password=(password)
+      @password = password
+
+      self.password_digest = BCrypt::Password.create(password)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'bcrypt'
 RSpec.describe User, type: :model do
   subject(:user) {
     User.create(name: 'SuccessGuy',
-    password_digest: BCrypt::Password.create('password'),
+    password: 'password',
     session_token: "AqnwDefnas" )
   }
 
@@ -13,4 +13,15 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:session_token) }
   it { should validate_uniqueness_of(:name) }
   it { should validate_uniqueness_of(:session_token) }
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  describe '#find_by_credentials' do
+    it 'should be able to take in name and password' do
+      expect{ User.find_by_credentials(name: 'name') }.to raise_error("Must provide password")
+      expect{ User.find_by_credentials(password: 'password') }.to raise_error("Must provide user_name")
+    end
+    it "should return a user" do
+      expect(User.find_by_credentials(name: 'SuccessGuy', password: 'password')).to be(User.first)
+    end
+  end
 end
